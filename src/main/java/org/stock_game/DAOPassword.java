be57688bb5f3a541e4f081d69ee66972f3e58fdd
Kamil <karaxek23@gmail.com>
2023-02-12 13:51:23 +0100

    changed DAOUser to DAOPassword

---
+++ src/main/java/org/stock_game/DAOPassword.java
@@ -0,0 +1,35 @@
+package org.stock_game;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DAOPassword {
+
+    private static DAOPassword instance;
+    private final DBConnection dbConnection;
+
+    private DAOPassword() {
+        dbConnection = DBConnection.getInstance();
+    }
+
+    public static DAOPassword getInstance() {
+        if (instance == null) {
+            instance = new DAOPassword();
+        }
+        return instance;
+    }
+
+    public String getPassword(String username) {
+        String sqlQuery = "SELECT hashed_password " +
+                "FROM public.\"Users\" " +
+                "WHERE username == '" + username + "'";
+        try {
+            ResultSet result = dbConnection.executeSelectQuery(sqlQuery);
+            String password = result.getString("hashed_password");
+            return password;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
