47a938bef4103e8ecc8da142f0b3f9429546bcac
Kamil Tylus <75392772+ktylus@users.noreply.github.com>
2023-02-16 15:23:02 +0100

    delete static analysis code

--- codemr/stock_game_project/html/main_report/htmlx/modeldata/packagedependency.js
+++
@@ -1,5 +0,0 @@
-var matrix = [[0]]
-var packages = [{
-"name": " org.stock_game", "color": " #3182bd"
-}
-];
--- codemr/stock_game_project/html/workingsets.js
+++
@@ -1,3 +0,0 @@
-var EQ_workingSetList = [
-{name: 'stock_game_project.main', path:'stock_game_projectmain'}
-];
