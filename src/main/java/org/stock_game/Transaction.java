c123a072777cb3ae11b20dcba6d76fa3dabc7988
Kamil <karaxek23@gmail.com>
2023-02-16 10:56:56 +0100

    price in Transaction now correctly displays the dollar sign

--- src/main/java/org/stock_game/Transaction.java
+++ src/main/java/org/stock_game/Transaction.java
@@ -6,6 +6,6 @@ import java.time.LocalDate;
 public record Transaction(String companyCode, int units, BigDecimal unitPrice, TransactionType type, LocalDate date) {
 
     public void print() {
-        System.out.println(companyCode + "  |  " + units + "  |  " + unitPrice + "  |  " + type + "  |  " + date);
+        System.out.println(companyCode + "  |  " + units + "  |  " + unitPrice + "$  |  " + type + "  |  " + date);
     }
 }
